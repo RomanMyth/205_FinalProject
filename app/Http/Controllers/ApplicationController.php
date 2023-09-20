ca690c40ebbfca61d00d288498664973f2493101
BananaBoi1232 <martin1420z@comcast.net>
2023-09-20 15:14:58 -0400

    Added view and changed default view

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Applications;
 
 class ApplicationController extends Controller
 {
-    //
+    function showAppForm(){
+        return view('applicationForm');
+    }
+
 }
