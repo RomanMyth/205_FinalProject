28acad11de438f8a8c5685cd39ab9058e2f2bbd3
Endermage44 <bad0529@icloud.com>
2023-09-21 16:00:54 -0400

    Added styling to applications view

--- resources/views/applications.blade.php
+++ resources/views/applications.blade.php
@@ -5,10 +5,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Job Applications</title>
+    <style>
+        h1{
+            display:flex;
+            justify-content:center;
+        }
+        #app_table{
+            display:flex;
+            justify-content:center;
+            border-collapse:collapse;
+        }
+        #app_table td, th {
+            border: 1px solid;
+            padding:5px 0px;
+        }
+        #app_table tbody{
+            width:75%;
+            display:flex;
+            flex-direction:column;
+            justify-content:space-evenly;
+        }
+        .app_table_row{
+            display:flex;
+        }
+        .app_table_row th{
+            width:12%;
+            display:flex;
+            justify-content:center;
+        }
+        .app_table_row td{
+            width:12%;
+            display:flex;
+            justify-content:center;
+        }
+    </style>
+
 </head>
 <body>
-    <table>
-        <tr>
+    <h1>Applications</h1>
+    <table id="app_table">
+        <tr class="app_table_row">
             <th>Application ID</th>
             <th>First Name</th>
             <th>Last Name</th>
@@ -19,23 +55,22 @@
             <th>Other Information</th>
         </tr>
 
-        <?php
+        
             
-            foreach ($applications as $application) {
-                print "<tr>";
-                    print "<td>".$application->application_id."</td>";
-                    print "<td>".$application->firstname."</td>";
-                    print "<td>".$application->lastname."</td>";
-                    print "<td>".$application->email."</td>";
-                    print "<td>".$application->address."</td>";
-                    print "<td>".$application->education."</td>";
-                    print "<td>".$application->years_of_experience."</td>";
-                    print "<td>".$application->other."</td>";
+            @foreach ($applications as $application) 
+                <tr class="app_table_row">
+                    <td>{{ $application->application_id }}</td>
+                    <td>{{ $application->firstname }}</td>
+                    <td>{{ $application->lastname }}</td>
+                    <td>{{ $application->email }}</td>
+                    <td>{{ $application->address }}</td>
+                    <td>{{ $application->education }}</td>
+                    <td>{{ $application->years_of_experience }}</td>
+                    <td>{{ $application->other }}</td>
 
-                print "</tr>";
-            }
-
-        ?>
+                </tr>
+            @endforeach
+        
     </table>
     
 </body>
