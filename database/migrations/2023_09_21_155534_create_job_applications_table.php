f2468d091bae54aec4aee51d00ff810dd1043970
RomanMyth <117763464+RomanMyth@users.noreply.github.com>
2023-09-21 12:28:54 -0400

    Addded Correct Model and fixed Create route

---
+++ database/migrations/2023_09_21_155534_create_job_applications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_applications', function (Blueprint $table) {
+            $table->id();
+            $table->integer('application_id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('address');
+            $table->string('education');
+            $table->integer('years_of_experience');
+            $table->string('other');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_applications');
+    }
+};
